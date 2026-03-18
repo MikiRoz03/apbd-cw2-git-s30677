b944c9ad1f20369c531e9654347968d4c6edf030
Mikołaj Rozwadowski <s30677@pjwstk.edu.pl>
2026-03-18 19:32:13 +0100

    Use average in Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Statistics app");
 int result = StatisticsHelper.Sum(2, 3);
 Console.WriteLine($"Sum: {result}");
+
+double average = StatisticsHelper.Average(new[] {1, 2, 3});
+Console.WriteLine($"Average: {average}");
