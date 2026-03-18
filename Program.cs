dfd3cbbb08f828b38ca6215fa43db0c36b7d78c2
Mikołaj Rozwadowski <s30677@pjwstk.edu.pl>
2026-03-18 20:20:36 +0100

    Use min in Program

--- Program.cs
+++ Program.cs
@@ -4,3 +4,9 @@ Console.WriteLine($"Sum: {result}");
 
 double average = StatisticsHelper.Average(new[] {1, 2, 3});
 Console.WriteLine($"Average: {average}");
+
+int max = StatisticsHelper.Max(new[] {1, 2, 3});
+Console.WriteLine($"Max: {max}");
+
+int min = StatisticsHelper.Min(new[] {1, 2, 3});
+Console.WriteLine($"Min: {min}");
